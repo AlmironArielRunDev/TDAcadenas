515be8a312be3fb40bc0e1bf42ee2b9af498a1dd
RenzoCagnola <rencagnola99@gmail.com>
2024-06-03 12:04:56 -0300

    correcciones

--- TDAcadenas/funciones/contar_palabras.py
+++
@@ -1,6 +0,0 @@
-def contar_palabras(texto):
-    palabras = texto.split()
-    
-    cantidad_palabras = len(palabras)
-    
-    return print(f"La página de libro ingresada contiene {cantidad_palabras} palabras.")
